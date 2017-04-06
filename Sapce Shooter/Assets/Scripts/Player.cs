5f6776b3306803c2eba141ad9a3a464215179dc8
yani-patrice <yani.patrice@yahoo.com>
2017-04-07 08:29:05 +1200

    Boundary added and player ship sprite implemented
    boundary by george
    sprite by yani

---
+++ Sapce Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin,xMax, zMin, zMax;
+}
+
+public class Player : MonoBehaviour {
+	public float speed;
+    // Use this for initialization
+    public Boundary boundary;
+    void Start () {
+		speed = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float axisX= Input.GetAxis ("Horizontal");
+		float axisY = Input.GetAxis ("Vertical");
+        Vector3 movement = new Vector3(axisX, 0.0f, axisY);
+        GetComponent<Rigidbody>().velocity = movement * speed;
+        GetComponent<Rigidbody>().position = new Vector3
+           (
+               Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+               0.0f,
+               Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+           );
+
+    }
+}
