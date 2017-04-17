ccda7d23a216a031f18e4375871aac9e999e88d6
yani-patrice <yani.patrice@yahoo.com>
2017-04-18 10:23:20 +1200

    Added destroy collision script

---
+++ Sapce Shooter/Assets/Scripts/DestroyHazardsByCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyHazardsByCollision : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Hazard") {
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
+
---
+++ Sapce Shooter/Assets/Scripts/DestroyWhenDone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public class DestroyWhenDone : MonoBehaviour
+{
+	public float timePassed;
+
+	void Start ()
+	{
+		Destroy (gameObject, timePassed);
+	}
+}
