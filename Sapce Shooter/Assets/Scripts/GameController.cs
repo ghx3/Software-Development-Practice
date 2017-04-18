c45f1e46a370e91824ca06770ad3d6ce9cb8ead1
George0225 <Georgeabc0225@outlook.com>
2017-04-18 12:10:03 +1200

    Add asteroid rotation

--- Sapce Shooter/Assets/Scripts/GameController.cs
+++ Sapce Shooter/Assets/Scripts/GameController.cs
@@ -9,22 +9,18 @@ public class GameController : MonoBehaviour {
 	public int asteroidCount;
 	public float spawnWait;
 	public float startWait;
-	public float waveWait;
 
 	void Start () {
 		StartCoroutine (SpawnAsteroid ());		
 	}
 
 	IEnumerator SpawnAsteroid () {
-		yield return new WaitForSeconds (startWait);
-		while (true) { //can change later when there is a set number of waves 
-			for (int i=0;i<asteroidCount;i++) {
-				Vector3 spawnPosition = new Vector3 (Random.Range (-positionValues.x, positionValues.x), positionValues.y, positionValues.z);
-				Quaternion spawnRotation = Quaternion.Euler (90, 0, 0);
-				Instantiate (asteroid, spawnPosition, spawnRotation);
-				yield return new WaitForSeconds (spawnWait);
-			}
-			yield return new WaitForSeconds (waveWait);
+		for (int i=0;i<asteroidCount;i++) {
+			Vector3 spawnPosition = new Vector3 (Random.Range (-positionValues.x, positionValues.x), positionValues.y, positionValues.z);
+			Quaternion spawnRotation = Quaternion.Euler (90, 0, 0);
+			Instantiate (asteroid, spawnPosition, spawnRotation);
+			yield return new WaitForSeconds (spawnWait);
+
 		}
 	}
 }
