7f6166bd1f24d8906ac5d109ebc1ed143941d0d0
George0225 <Georgeabc0225@outlook.com>
2017-04-18 14:38:57 +1200

    Update the asteroid rotation

---
+++ Sapce Shooter/Assets/Scripts/Rotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+        public Vector3 Velocity;
+        public Rigidbody rb;
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+        void Update()
+        {
+            Quaternion rotation = Quaternion.Euler(Velocity * Time.deltaTime);
+            rb.MoveRotation(rb.rotation * rotation);
+        }
+    }
