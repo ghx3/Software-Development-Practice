1d6e4b74faf42ad949800f0091772d3722099faa
Mr.G <sodaverdelvy@gmail.com>
2017-05-03 08:51:19 +1200

    minor bugs fixed

--- Sapce Shooter/Assets/Scripts/PauseButton.cs
+++ Sapce Shooter/Assets/Scripts/PauseButton.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseButton : MonoBehaviour {
-	GameObject[] pauseObjects;
+	
 
 	// Use this for initialization
 	void Start () {
 		Time.timeScale = 1;
-		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
-		hidePaused();
+
+
 	}
 
 	// Update is called once per frame
@@ -21,11 +21,11 @@ public class PauseButton : MonoBehaviour {
 			if(Time.timeScale == 1)
 			{
 				Time.timeScale = 0;
-				showPaused();
+			
 			} else if (Time.timeScale == 0){
-				Debug.Log ("high");
+
 				Time.timeScale = 1;
-				hidePaused();
+			
 			}
 		}
 	}
@@ -38,26 +38,14 @@ public class PauseButton : MonoBehaviour {
 		if(Time.timeScale == 1)
 		{
 			Time.timeScale = 0;
-			showPaused();
+
 		} else if (Time.timeScale == 0){
 			Time.timeScale = 1;
-			hidePaused();
+	
 		}
 	}
 
-	//shows objects with ShowOnPause tag
-	public void showPaused(){
-		foreach(GameObject g in pauseObjects){
-			g.SetActive(true);
-		}
-	}
 
-	//hides objects with ShowOnPause tag
-	public void hidePaused(){
-		foreach(GameObject g in pauseObjects){
-			g.SetActive(false);
-		}
-	}
 
 
 
