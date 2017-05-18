5374a95c812cc3e9dbbf7652abe0fc010288d9d7
George0225 <Georgeabc0225@outlook.com>
2017-05-18 18:45:31 +1200

    Add basic energy bar

---
+++ Sapce Shooter/Assets/Scripts/EnergyBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyBar : MonoBehaviour {
+    public float maxEnergy=100f;
+    public float currentEnergy=0f;
+    public GameObject Energybar;
+
+	// Use this for initialization
+	void Start () {
+        currentEnergy = 0f;
+        InvokeRepeating("addEnergy",1f,1f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void addEnergy()
+    {
+        currentEnergy += 2f;
+        float calEnergy = currentEnergy/maxEnergy;
+        setEnergyBar(calEnergy);
+    }
+    public void setEnergyBar(float myEnergy)
+    {
+        Energybar.transform.localScale = new Vector3(Mathf.Clamp(myEnergy, 0f, 1f), Energybar.transform.localScale.x, Energybar.transform.localScale.z);
+    }
+}
+
